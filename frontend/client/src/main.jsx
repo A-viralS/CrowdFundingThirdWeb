0f5446fcb9867dca5ede0554f65c386824b7a664
Aviral Soni <aviralsoni@Avirals-MacBook-Air.local>
2024-01-10 20:10:26 +0530

    basic frontend

---
+++ frontend/client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter as Router
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App'; // Make sure to import your App component
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // the ThirdwebProvider will be the wrapper and will take the chainId
+  <ThirdwebProvider desiredChainId={11155111}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
