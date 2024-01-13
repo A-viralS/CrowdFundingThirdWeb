baa21ea29643d0af556ae4b550d431f004a09aec
Aviral Soni <aviralsoni@Avirals-MacBook-Air.local>
2024-01-13 14:47:59 +0530

    m3

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <Navbar/> 
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/fontend/client" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-campaign" element={<CreateCampaign />} />
           <Route path="/campaign-details/:id" element={<CampaignDetails />} />
